69801bd81144e934874d3b349c868a65176ed79c
Eliah Diener <dieneeli@students.zhaw.ch>
2026-03-04 11:31:07 +0100

    Test

---
+++ functions/Einheiten.py
@@ -0,0 +1,10 @@
+def umrechnen(meter):
+
+    return meter * 100 
+
+
+def tage_in_stunden_und_minuten(tage):
+   
+    stunden = tage * 24
+    minuten = stunden * 60
+    return stunden, minuten
--- views/unterseite_a.py
+++ views/unterseite_a.py
@@ -1,5 +1,35 @@
 import streamlit as st
 
-st.title("Unterseite A")
+st.title("Umrechnen")
 
-st.write("Diese Seite ist eine Unterseite der Startseite.")
+st.write("Einheiten umrechnen von Meter in Centimeter")
+
+
+from functions.Einheiten import umrechnen
+
+
+try:
+    eingabe = st.number_input("Metern eingeben: ")
+    
+    m = float(eingabe)
+    cm = umrechnen(m)
+    print(f"{m} m entsprechen {cm} cm")
+except ValueError:
+    print("Bitte eine gültige Zahl eingeben.")
+    submit = st.button("umrechnen")
+    if submit:
+        st.write("Resultat")
+
+
+try:
+    eingabe = st.number_input("Tage eingeben: ")
+    m = float(eingabe)
+    cm = umrechnen(m)
+    print(f"{m} m entsprechen {cm} cm")
+except ValueError:
+    print("Bitte eine gültige Zahl eingeben.")
+    submit = st.button("umrechnen")
+    if submit:
+        st.write("Resultat")
+
+
